4b32a6af954ceea2a74b02b9fa8c9b9ab09aefe0
sam1096 <Samridhi.Kundaliya@iiitb.org>
2019-01-17 10:29:25 +0530

    included subtraction module

---
+++ Subtract.java
@@ -0,0 +1,14 @@
+public class Subtract
+{
+	float a,b,res;
+	Multiply(float x,float y)
+	{
+		a=x;
+		b=y;
+	}
+	public void getRes()
+	{
+		res=a-b;
+		System.out.println("The result of multiplication is: "+res);
+	}
+}
